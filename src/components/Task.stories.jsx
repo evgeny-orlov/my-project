64f9fb09f7ad22a3a70cc9bde3e8c066b4f128d5
Evgeny <evgentusus@gmail.com>
2024-04-05 12:30:31 +0300

    first commit

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -34,3 +34,33 @@ export const Archived = {
     },
   },
 };
+
+export const TitleNull = {
+  args: {
+    task: {
+      id: '1',
+      title: '',
+      state: 'TASK_ARCHIVED', 
+    },
+  },
+};
+
+export const LongText = {
+  args: {
+    task: {
+      id: '110',
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus nisl ut iaculis venenatis. Donec fermentum nisl quam, vel suscipit felis convallis et. Praesent malesuada elit vitae suscipit finibus. Nam ut dignissim tortor. Aenean in vestibulum leo, et aliquam nibh. Praesent at nisl ex. Morbi eleifend sagittis metus in interdum. Donec vulputate ex ut metus pulvinar porttitor. Morbi et odio eu ex lobortis mattis et eget metus.',
+      state: '', 
+    },
+  },
+};
+
+export const Default3 = {
+  args: {
+    task: {
+      id: '1',
+      title: 'Введите текст',
+      state: 'TASK_PINNED'
+    },
+  },
+};
